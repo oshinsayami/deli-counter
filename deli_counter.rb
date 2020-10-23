5e42c0704cf6ca1e320545fbd2ce19be07057d37
Simran <mdrsimran@gmail.com>
2020-10-22 21:29:09 -0500

    commit tree afbf10a6b6f2caeaf40f4ff821c02f8c6371bbba

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+require "pry"
+katx_deli=[]
+
+def line(katz_deli)
+    if katz_deli.size==0
+        puts "The line is currently empty."
+    else
+    line= "The line is currently:"
+    count=1
+    new_line=""
+    katz_deli.each do |name|
+        new_line = new_line + " #{count}. #{name}"
+        count +=1
+
+        # binding.pry
+    end
+    puts line +new_line
+    end
+end
+
+def take_a_number(katz_deli, name)
+katz_deli.push(name)
+puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+else
+    puts "Currently serving #{katz_deli.shift}."
+end
+end
